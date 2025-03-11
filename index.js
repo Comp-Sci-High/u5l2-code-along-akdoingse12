fe0b0111a9bbfa6fef9684e3d97dbdb5aaef010d
akdoingse12 <akdoingse12@gmail.com>
2025-03-11 14:30:30 +0000

    hbefd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // import your mongoose
-
+const mongoose = require("mogoose");
 // install your mongoose
 
 const app = express();
@@ -11,9 +11,13 @@ app.use((req, res, next) => {
 });
 
 // create a studentSchema with name, grade, advisory, and fav subject
-
+const studentSchema = new mongoose.studentSchema({
+  name: {type: String, require: true},
+  grade: {type: Number, default: 9},
+  favSub: {type: String}
+})
 // connect your schema to a model called Student
-
+const Student = mongoose.Model("Student", studentSchema, "Students")
 // create a route hanlder for /g12 that returns every student in grade 12
 
 // (OYO) create a route hanlder for /me that returns yourself without using your name in the query
@@ -27,6 +31,11 @@ app.use((req, res, next) => {
 // (OYO) save 2 more documents about students at your table
 // make sure to start your server 
 
+async function startServer() {
+  await mongoose.connect("mongodb+srv://akoladeajibola25:<db_password>@akdoingse12.4hcgu.mongodb.net/?retryWrites=true&w=majority&appName=akdoingse12"  
+
+    const me = await new Student()
+  )}
 
 // call startServer
 
